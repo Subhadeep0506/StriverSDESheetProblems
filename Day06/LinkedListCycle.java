922fc5039676f6b169bdbb449ae01835f9e677d8
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2022-08-02 00:49:48 +0530

    Added 4 Day07 problems

---
+++ Day06/LinkedListCycle.java
@@ -0,0 +1,35 @@
+/*
+ * Given head, the head of a linked list, determine if the linked list has a cycle in
+ * it.
+ * 
+ * There is a cycle in a linked list if there is some node in the list that can be
+ * reached again by continuously following the next pointer. Internally, pos is used to
+ * denote the index of the node that tail's next pointer is connected to. Note that pos
+ * is not passed as a parameter.
+ * 
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ */
+
+package Day06;
+
+import Day05.*;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if (fast == slow)
+                return true;
+        }
+        
+        return false;
+    }
+}
---
+++ Day06/LinkedListCycleII.java
@@ -0,0 +1,39 @@
+/*
+ * Given the head of a linked list, return the node where the cycle begins. If there is
+ * no cycle, return null.
+ * 
+ * There is a cycle in a linked list if there is some node in the list that can be
+ * reached again by continuously following the next pointer. Internally, pos is used to
+ * denote the index of the node that tail's next pointer is connected to (0-indexed).
+ * It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+ * 
+ * Do not modify the linked list.
+ */
+
+package Day06;
+
+import Day05.ListNode;
+
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+        
+        ListNode slow = head, fast = head;
+        
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        
+        return null;
+    }
+}
