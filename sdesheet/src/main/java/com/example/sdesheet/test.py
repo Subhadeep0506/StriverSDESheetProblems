597e8c1aa4b4b1b6583020d41d663aeb363aa7d0
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2022-08-26 02:17:42 +0530

    Added day25 problems

--- sdesheet/src/main/java/com/example/sdesheet/test.py
+++ sdesheet/src/main/java/com/example/sdesheet/test.py
@@ -1,23 +1,19 @@
-def maxMin(k, arr):
-    # Write your code here
-    arr.sort()
-    arr_copy = []
-    i = 0
-    count = 1
-    lowest_diff = 9999
-    arr_copy.append(arr[0])
-    for i in range(1, len(arr)):
-        diff = arr[i] - arr_copy[-1] 
-        if diff < lowest_diff:
-            diff = lowest_diff
-            arr_copy.append(arr[i])
+    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
+        m = len(s1)
+        n = len(s2)
+        memo = [[-1 for _ in range(n+1)] for _ in range(m+1)]
+        return self.helper(s1,s2,0,0,memo)
         
-    res = max(arr_copy) - min(arr_copy)
-    
-    return res
-
-arr = [100, 200, 300, 350, 400, 401, 402]
-k = 2
-i = 1e-6
-
-print(type(i))
+    def helper(self,s1,s2,i,j,memo):	
+	    #not a visited subproblem
+	    if memo[i][j] < 0:
+		#base case - if end of strings are reached, longest len will be 0
+		    if i == len(s1) or j == len(s2):
+			    memo[i][j] = 0
+		    elif s1[i] == s2[j]:
+			    memo[i][j] = 1 + self.helper(s1,s2,i+1,j+1,memo)
+		    else:
+			    memo[i][j] = 0 + max(self.helper(s1,s2,i+1,j,memo),
+						   self.helper(s1,s2,i,j+1,memo)
+						   )
+	    return memo[i][j]
