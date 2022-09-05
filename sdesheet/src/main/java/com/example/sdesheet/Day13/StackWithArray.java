35fbac906eb33996ee586789b12e27ba9f7d035e
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2022-09-06 00:31:38 +0530

    Added all day12 problems

---
+++ sdesheet/src/main/java/com/example/sdesheet/Day13/StackWithArray.java
@@ -0,0 +1,52 @@
+package com.example.sdesheet.Day13;
+
+public class StackWithArray {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(3);
+        stack.push(12);
+        stack.push(2);
+        stack.push(9);
+        System.out.println(stack.pop());
+        stack.push(7);
+        stack.push(4);
+        System.out.println(stack.top());
+        stack.push(6);
+    }
+}
+
+class Stack {
+    int size;
+    int arr[];
+    int top;
+
+    public Stack() {
+        this.size = 10;
+        arr = new int[size];
+    }
+
+    public Stack(int size) {
+        this.size = size;
+        arr = new int[size];
+    }
+
+    public void push(int num) {
+        if (size() == 10)
+            size += 10;
+
+        arr[++top] = num;
+    }
+
+    public int pop() {
+        return arr[top--];
+    }
+
+    public int top() {
+        return arr[top];
+    }
+
+    public int size() {
+        return this.size;
+    }
+}
--- sdesheet/src/main/java/com/example/sdesheet/Test.java
+++ sdesheet/src/main/java/com/example/sdesheet/Test.java
@@ -3,5 +3,7 @@ package com.example.sdesheet;
 import java.util.*;
 
 class Test {
-    
+    public static void main(String args[]) {
+        
+    }
 }
