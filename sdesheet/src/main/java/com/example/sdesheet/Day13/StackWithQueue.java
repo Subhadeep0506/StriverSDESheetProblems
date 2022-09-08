4e42894f572170f3f75b28bd5165f88d6ce4a0bb
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2022-09-09 01:26:30 +0530

    Added Day13 problems

---
+++ sdesheet/src/main/java/com/example/sdesheet/Day13/StackWithQueue.java
@@ -0,0 +1,29 @@
+package com.example.sdesheet.Day13;
+
+import java.util.*;
+
+public class StackWithQueue {
+	Queue<Integer> queue;
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        int size = queue.size();
+        queue.offer(x);
+        for (int i = 0; i < size; i++)
+            queue.offer(queue.poll());
+    }
+    
+    public int pop() {
+        return queue.poll();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
