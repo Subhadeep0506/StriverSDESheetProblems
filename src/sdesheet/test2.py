f990460edb2a29362b7739a4a04d767e39fea4b8
Subhadeep Mandal <subahdeepdoublecap@gmail.com>
2023-01-09 10:04:27 +0530

    Added bit manupulation programs

--- src/sdesheet/test2.py
+++ src/sdesheet/test2.py
@@ -1,42 +1,12 @@
-def collectCoins(coins):
-    pick1, pick2 = 0, 0
+def isNLPCompatible(string):
+    string = string.lower()
+    chars = [0] * 26
+    for c in string:
+        if c.isalpha() and chars[ord(c) - ord('a')] == 0:
+            chars[ord(c) - ord('a')] += 1
 
-    for coin in coins:
-        temp = max(coin + pick1, pick2)
-        pick1 = pick2
-        pick2 = temp
-    
-    return pick2
+    return 1 if sum(chars) >= 26 else 0
 
 
-def collect(coins):
-    size = len(coins) + 1
-    dp = [-1] * size
-
-    def helper(index):
-        if index < 0:
-            return 0
-        elif index == 0:
-            return coins[0]
-        if dp[index] != -1:
-            return dp[index]
-
-        take = helper(index - 2) + coins[index]
-        notTake = helper(index - 1)
-        dp[index] = max(take, notTake)
-        return dp[index]
-
-    return helper(size - 2)
-
-if __name__ == "__main__":
-    size = int(input())
-    coins = []
-    for _ in range(size):
-        coins.append(int(input()))
-    
-    print(collect(coins))
-
-"""
-[pick1, pick2, n, n + 1, ...]
-
-"""
+string = "Apples were served as the dessert"
+print(isNLPCompatible(string))
